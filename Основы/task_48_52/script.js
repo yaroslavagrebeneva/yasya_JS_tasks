592c451ae173190cd0e8deaacf8439771527af0a
anarhisssttt <grebeneva.y.i.23@gmail.com>
2024-10-02 13:24:34 +0300

    task#49

--- Основы/task_48_52/script.js
+++ Основы/task_48_52/script.js
@@ -2,3 +2,13 @@
 let a48 = 1;
 let b48 = 95;
 alert(String(a48) + String(b48)); 
+
+// Задание 49 №1
+let a49_1 = 545;
+alert(String(a49_1).length);
+
+// Задание 49 №2
+let a49_2 = 55; 
+let b49_2 = 245;   
+let sum49_2 = String(a49_2).length + String(b49_2).length;
+alert(sum49_2);
