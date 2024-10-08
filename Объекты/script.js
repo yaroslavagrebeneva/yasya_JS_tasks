8e06d8c4b2c93706684734098242999234a5bb49
anarhisssttt <grebeneva.y.i.23@gmail.com>
2024-10-08 16:27:36 +0300

    task#91

--- Объекты/script.js
+++ Объекты/script.js
@@ -89,3 +89,12 @@ console.log(Object.keys(obj89).length);
 let obj90 = {x: 1, y: 2, z: 3};
 let key90 = 'x';
 console.log(obj90[key90]);
+
+/*task#91*/
+
+let obj91_1 = {x: 1, y: 2, z: 3};
+console.log(obj91_1['x']);
+
+let obj91_2 = {x: 1, y: 2, z: 3};
+let key91 = 'x';
+console.log(obj91_2[key91]);
