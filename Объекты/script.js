23cc3725a8d47464b43cb7fbba2fab9c9723e198
anarhisssttt <grebeneva.y.i.23@gmail.com>
2024-10-08 16:19:56 +0300

    task#85

--- Объекты/script.js
+++ Объекты/script.js
@@ -49,3 +49,11 @@ console.log(obj84_2.b2);
 console.log(obj84_2['c-c']);
 console.log(obj84_2['d 4']);
 console.log(obj84_2.e5);
+
+/*task#85*/
+
+let obj85 = {x: 1, y: 2, z: 3};
+obj85.x = obj85.x ** 2;
+obj85.y = obj85.y ** 2;
+obj85.z = obj85.z ** 2;
+console.log(obj85);
