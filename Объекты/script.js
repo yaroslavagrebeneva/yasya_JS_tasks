715aef36c279d54149624ed6ad61c476f8e4d45e
anarhisssttt <grebeneva.y.i.23@gmail.com>
2024-10-08 16:32:46 +0300

    task#96

--- Объекты/script.js
+++ Объекты/script.js
@@ -139,3 +139,22 @@ let obj95 = {x: 1, y: 2, z: 3};
 delete obj95.x;
 console.log('x' in obj95);
 // Результат вывода = {y: 2, z: 3}
+
+/*task#96*/
+//  №1
+// console.log( typeof {x: 1, y: 2, z: 3} );
+// Результат вывода =  object
+
+// №2
+// console.log( typeof {} );
+// Результат вывода =  object
+
+// №3
+// let obj = {x: 1, y: 2, z: 3};
+// console.log( typeof obj );
+// Результат вывода =  object
+
+// №4
+// let obj1 = {x: 1, y: 2, z: 3};
+// console.log( typeof obj1['x'] ); 
+// Результат вывода = number
