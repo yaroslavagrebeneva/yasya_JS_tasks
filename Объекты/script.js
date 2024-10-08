f7aa46fa036199baf8bf4ca9e3c744d1ed97b0ce
anarhisssttt <grebeneva.y.i.23@gmail.com>
2024-10-08 16:14:46 +0300

    task#81

--- Объекты/script.js
+++ Объекты/script.js
@@ -8,3 +8,19 @@ let obj80 =
 6:"Saturday", 
 7:"Sunday"};
 console.log(obj80[1], obj80[2], obj80[3], obj80[4], obj80[5], obj80[6], obj80[6]);
+
+/*task#81*/
+let obj81 = 
+{1: 'January', 
+2: 'February', 
+3: 'March', 
+4: "April", 
+5: 'May', 
+6: 'June', 
+7: 'July', 
+8: 'August', 
+9: 'September', 
+10: 'October', 
+11: 'November', 
+12: 'December'};
+console.log(obj81);
