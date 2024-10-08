609dd35732d42d3ee2cc046f2ce1a77d77203b36
anarhisssttt <grebeneva.y.i.23@gmail.com>
2024-10-08 16:13:08 +0300

    task#80

---
+++ Объекты/script.js
@@ -0,0 +1,10 @@
+/*task#80*/
+let obj80 = 
+{1: 'Monday', 
+2: 'Tuesday',
+3: 'Wednesday', 
+4:'Thursday', 
+5:'Friday', 
+6:"Saturday", 
+7:"Sunday"};
+console.log(obj80[1], obj80[2], obj80[3], obj80[4], obj80[5], obj80[6], obj80[6]);
