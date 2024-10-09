d3f71ec4ef1a36aa9286d1bc52895dc5ad81c98e
anarhisssttt <grebeneva.y.i.23@gmail.com>
2024-10-09 12:28:18 +0300

    task#97

---
+++ Объекты/task_97_103/script.js
@@ -0,0 +1,24 @@
+/*task#97*/
+
+// №1
+// console.log( typeof {x: 1, y: 2, z: 3} ); 
+// Результат вывода = object
+
+// №2
+// console.log( typeof [1, 2, 3] ); 
+// Результат вывода = object
+
+// №3
+// let arr1 = [1, 2, 3];
+// console.log( typeof arr1 ); 
+// Результат вывода = object
+
+// №4
+// let arr2 = [1, 2, 3];
+// console.log( typeof arr2[0] ); 
+// Результат вывода = number
+
+// №5
+// let arr3 = ['1', '2', '3'];
+// console.log( typeof arr3[0] ); 
+// Результат вывода = string
