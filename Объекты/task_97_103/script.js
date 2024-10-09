49011dab25432fdc72611caa042dacbf9370093f
anarhisssttt <grebeneva.y.i.23@gmail.com>
2024-10-09 12:40:19 +0300

    task#103

--- Объекты/task_97_103/script.js
+++ Объекты/task_97_103/script.js
@@ -126,3 +126,23 @@ console.log(arr101_3);
 const arr102 = [1, 2, 3, 4, 5];
 const res102 = arr102[1] + arr102[2];
 console.log(res102);
+
+/*task#103*/
+
+//№1
+let obj103_1 = {x: 1, y: 2, z: 3};
+console.log(objobj103_1['x']); 
+
+//№2
+let obj103_2 = {x: 1, y: 2, z: 3};
+let key103_2  = 'x';
+console.log(obj103_2[key103_2 ]);
+
+//№3
+let obj103_3 = {x: 1, y: 2, z: 3};
+let sum = obj103_3['x'] + obj103_3['y'] + obj103_3['z']; 
+console.log(sum); 
+
+//№4
+let obj103_4 = {x: 1, y: 2, z: 3};
+console.log(Object.keys(obj103_4).length); 
