367057fda86a139958d2601fcf1c737dfde28b24
anarhisssttt <grebeneva.y.i.23@gmail.com>
2024-10-15 16:00:39 +0300

    task#129

--- Условия/task_126_142/script.js
+++ Условия/task_126_142/script.js
@@ -32,3 +32,20 @@ if ((num128 >= 0) && (num128 <= 99)) {
 		console.log('Сумма цифр двузначна');
 	}
 } 
+
+/*task#129*/
+let lang = 'ru';
+switch (lang) {
+    case 'ru':
+		console.log('рус');
+	break;
+    case 'en':
+        console.log('анг');
+    break;
+    case 'de':
+		console.log('нем');
+	break;
+    default:
+		console.log('язык не поддерживается');
+	break;
+}
