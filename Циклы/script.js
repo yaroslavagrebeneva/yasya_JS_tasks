8c2af99c98a2d0406eaaa51f04edb363bfca666d
anarhisssttt <grebeneva.y.i.23@gmail.com>
2024-10-16 17:54:47 +0300

    task#145

--- Циклы/script.js
+++ Циклы/script.js
@@ -5,3 +5,21 @@ let arr144 = ['a', 'b', 'c', 'd', 'e'];
 for (let elem144 of arr144) {
 	console.log(elem144);
 }
+
+/*145*/
+
+// #1
+console.log('Результат задания 145 №1');
+let obj145_1 = {x: 1, y: 2, z: 3};
+console.log('Ключи объекта:'); 
+for (let key145_1 in obj145_1) {
+	console.log(key145_1); 
+}
+
+// #2
+console.log('Результат задания 145 №2');
+let obj145_2 = {x: 1, y: 2, z: 3};
+console.log('Элементы объекта:'); 
+for (let key145_2 in obj145_2) {
+	console.log(obj145_2[key145_2]); 
+}
