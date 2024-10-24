fd8a94701f76b2e54f27ffd8e7c35882ed0201a5
anarhisssttt <grebeneva.y.i.23@gmail.com>
2024-10-24 14:49:37 +0300

    task#179

--- Многомерность/script.js
+++ Многомерность/script.js
@@ -527,3 +527,107 @@ let students = {
 	'group3': ['Baskov', 'Gagarina', 'Karnaval'],
 };
 console.log(students['group1'][0]);
+
+/*179*/
+// #1
+console.log('Результат задания 179 №1:');
+let data = {
+	1: [
+		'data11',
+		'data12',
+		'data13',
+	],
+	2: [
+		'data21',
+		'data22',
+		'data23',
+	],
+	3: [
+		'data31',
+		'data32',
+		'data33',
+	],
+	4: [
+		'data41',
+		'data42',
+		'data43',
+	],
+};
+for (let group in data) {
+	for (let name of data[group]) {
+		console.log(name);
+	}
+}
+
+// #2
+console.log('Результат задания 179 №2:');
+data = [
+	{
+		1: 'data11',
+		2: 'data12',
+		3: 'data13',
+	},
+	{
+		1: 'data21',
+		2: 'data22',
+		3: 'data33',
+	},
+	{
+		1: 'data31',
+		2: 'data32',
+		3: 'data33',
+	},
+];
+for (let group of data) {
+	for (let name in group) {
+		console.log(group[name]);
+	}
+}
+
+// #3
+console.log('Результат задания 179 №3:');
+let data3 = [
+  {
+    1: [
+      'data111',
+      'data112',
+      'data113',
+    ],
+    2: [
+      'data121',
+      'data122',
+      'data123',
+    ],
+  },
+  {
+    1: [
+      'data211',
+      'data212',
+      'data213',
+    ],
+    2: [
+      'data221',
+      'data222',
+      'data223',
+    ],
+  },
+  {
+    1: [
+      'data411',
+      'data412',
+      'data413',
+    ],
+    2: [
+      'data421',
+      'data422',
+      'data423',
+    ],
+  },
+];
+for (let i = 0; i < data3.length; i++) {
+  let subObj = data[i]; 
+  for (let key in subObj) {
+    let items = subObj[key];
+    console.log(items); 
+  }
+}
