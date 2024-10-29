08330ef5b030ec38cfff8feed0557223cc210b99
anarhisssttt <grebeneva.y.i.23@gmail.com>
2024-10-29 10:23:47 +0300

    task#197

--- Стандартные_методы/script.js
+++ Стандартные_методы/script.js
@@ -194,3 +194,9 @@ arr196_3.splice(2, 0, 'a', 'b');
 arr196_3.splice(6, 0, 'c');
 arr196_3.splice(8, 0, 'e');
 console.log(arr196_3);
+
+/*197*/
+console.log('Результат задания 197 №1:');
+let arr197 = [1, 2, 3, 4, 5];
+let res197 = arr197.includes(3);
+console.log(res197);
