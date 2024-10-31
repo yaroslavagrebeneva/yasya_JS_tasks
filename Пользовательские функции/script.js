57e40ae4fcad415320e8bdafd5007c72426426d6
anarhisssttt <grebeneva.y.i.23@gmail.com>
2024-10-31 19:41:25 +0300

    task#200

---
+++ Пользовательские функции/script.js
@@ -0,0 +1,18 @@
+/*200*/
+// #1
+console.log('Результат задания 200 №1:');
+function func() {
+	console.log('Katushka');
+}
+func();
+
+// #2
+console.log('Результат задания 200 №2:');
+function res200_2() {
+	let sum200_2 = 0;
+	for(let i = 1; i <= 100; i++){		
+		sum200_2 += i;
+	}
+  console.log(sum200_2);
+}
+res200_2();
