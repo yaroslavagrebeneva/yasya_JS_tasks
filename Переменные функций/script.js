c9d169411fea433e1b96ddd4facc325642a1e19a
anarhisssttt <grebeneva.y.i.23@gmail.com>
2024-11-06 16:02:41 +0300

    task#217-219

--- Переменные функций/script.js
+++ Переменные функций/script.js
@@ -23,3 +23,104 @@
 // func(); // Результат выполнения задания = 1
 // num = 2;
 // func(); // Результат выполнения задания = 2
+
+/*217*/
+
+// Задание 217 №1
+// function func() {
+// 	let num = 5;
+// 	return num;
+// }
+// console.log(num); // Результат выполнения задания = выдаст ошибку
+
+// Задание 217 №2
+// function func() {
+// 	let num = 5;
+// 	return num;
+// }
+// console.log(func()); // Результат выполнения задания = выдаст ошибку
+
+/*218*/
+
+// Задание 218 №1
+// let num = 1;
+// function func() {
+// 	num = 2;
+// }
+// func();
+// console.log(num); // Результат выполнения задания = 2
+
+// Задание 218 №2
+// let num = 1;
+// function func() {
+// 	let num = 2;
+// }
+// func();
+// console.log(num); // Результат выполнения задания = 1
+
+/*219*/
+
+// Задание 219 №1
+// let num = 1;
+// function func() {
+//   num = 2;
+// }
+// func();
+// console.log(num); // Результат выполнения задания = 2
+
+// Задание 219 №2
+// let num = 1;
+// function func() {
+//   num = 2;
+// }
+// console.log(num); // Результат выполнения задания = 1
+
+// Задание 219 №3
+// let num = 1;
+// function func() {
+//   let num = 2;
+// }
+// func();
+// console.log(num); // Результат выполнения задания = 1 
+
+// Задание 219 №4
+// let num = 1;
+// function func() {
+//   let num = 2;
+// }
+// console.log(num);
+// func(); // Результат выполнения задания = 1 
+
+// Задание 219 №5
+// let num = 1;
+// function func() {
+//   num = 2;
+// }
+// console.log(num);
+// func(); // Результат выполнения задания = 1 
+
+// Задание 219 №6
+// let num = 1;
+// function func() {
+//   num++;
+// }
+// func();
+// func();
+// func();
+// console.log(num); // Результат выполнения задания = 4
+
+// Задание 219 №7
+// function func() {
+//   num = 2;
+// }
+// let num = 1;
+// console.log(num);
+// func(); // Результат выполнения задания = 1
+
+// Задание 219 №8
+// function func() {
+//   num = 2;
+// }
+// let num = 1;
+// func();
+// console.log(num); // Результат выполнения задания = 2
