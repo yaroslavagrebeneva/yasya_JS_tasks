816d680e4dee51d9f8405ce1136e0c42d312e334
anarhisssttt <grebeneva.y.i.23@gmail.com>
2024-11-06 16:00:07 +0300

    task#216

---
+++ Переменные функций/script.js
@@ -0,0 +1,25 @@
+/*216*/
+// №1
+// let num = 1;
+// function func() {
+//     console.log(num);
+// }
+// func(); //Результат выполнения задания = 1
+
+// №2
+// let num = 1;
+// function func() {
+//     console.log(num);
+// }
+// num = 2;
+// func(); // Результат выполнения задания = 2  
+
+// №3
+// function func() {
+//     console.log(num);
+// }
+// let num;
+// num = 1;
+// func(); // Результат выполнения задания = 1
+// num = 2;
+// func(); // Результат выполнения задания = 2
